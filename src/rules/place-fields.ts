04d9fdfb2a682b54e8a633bef81f6c0313c215fd
Justin Poehnelt <jpoehnelt@google.com>
2021-12-20 16:33:10 -0700

    fix: regex terminate after Autocomplete (#20)

--- src/rules/place-fields.ts
+++ src/rules/place-fields.ts
@@ -43,7 +43,7 @@ export default createRule({
         try {
           if (
             fullNamespace(node.callee).match(
-              /google\.maps\.places\.Autocomplete/
+              /google\.maps\.places\.Autocomplete$/
             )
           ) {
             const references = context.getScope().references;
